4661d0d817b539c58d9e5f7e98a3acdbe5c183a9
Mickey Barron <mickeyb135@gmail.com>
2020-05-27 16:30:35 -0400

    Switched checkWins to use arrays, added AI hooks
    Co-authored-by: osullivanj@emmanuel.edu <osullivanj@emmanuel.edu>
    Co-authored-by: Mark Sherman <shermanm@emmanuel.edu>

--- Tic_Tac_Toe/ai.js
+++ Tic_Tac_Toe/ai.js
@@ -1,8 +1,8 @@
 // Here are the tools to use to build your AI:
 
-
-// The move() function is called by the game when it is the AI's turn to make a move. 
-// This function should return which square to play to, 0-8. 
-function AiMove() {
-    return 1;
+// The aiSelect() function is called by the game when it is the AI's turn to make a move.
+// This function should return which square to play to, 0-8.
+function aiSelect() {
+  print( getAvailableCells());
+  return random(getAvailableCells());
 }
--- Tic_Tac_Toe/sketch.js
+++ Tic_Tac_Toe/sketch.js
@@ -4,7 +4,7 @@
 
 var devMode = true;
 var board = [];
-var players = ['X', 'O'];
+var players = ["X", "O"];
 var currentPlayer = null;
 var availableCells = [];
 var gridSize = 3;
@@ -12,166 +12,196 @@ var canvasWidth = 500;
 var canvasHeight = canvasWidth;
 var columnWidth = canvasWidth / gridSize;
 var rowHeight = canvasHeight / gridSize;
-var winStatus = '';
+var winStatus = "";
 var player;
 
-function generateBoard(){
-    for (let i = 0; i < gridSize; i++){
-        let templateRow = [];
-        for (let j = 0; j < gridSize; j++){
-            templateRow.push('');
-        }
-        board.push(templateRow);
+function generateBoard() {
+  for (let i = 0; i < gridSize; i++) {
+    let templateRow = [];
+    for (let j = 0; j < gridSize; j++) {
+      templateRow.push("");
     }
-    for (let j = 0; j < gridSize; j++){
-        for (let i = 0; i < gridSize; i++){
-            availableCells.push([i,j]);
-        }
+    board.push(templateRow);
+  }
+  for (let j = 0; j < gridSize; j++) {
+    for (let i = 0; i < gridSize; i++) {
+      availableCells.push([i, j]);
     }
+  }
 }
 
-function checkWin(direction){
-    let pathResult;
-    let headerCell;
-    let comparisonCell;
-    if (direction == "diagonal"){
-        for (let i = -1; i <= 1; i = i + 2){
-            let yCoord = (((gridSize-1)/2)*(1+i));
-            headerCell = board[yCoord][0];
-            if (headerCell != ""){
-                pathResult = true;
-                for (let j = 1; j < gridSize; j++){
-                    let compY = yCoord + j*-i;
-                    comparisonCell = board[compY][j];
-                    if (comparisonCell != headerCell){
-                        pathResult = false;
-                    }
-                }
-                if (pathResult == true){return headerCell;}
-            }
+function checkWin(direction) {
+  let pathResult;
+  let headerCell;
+  let comparisonCell;
+  if (direction == "diagonal") {
+    for (let i = -1; i <= 1; i = i + 2) {
+      let yCoord = (gridSize - 1) / 2 * (1 + i);
+      headerCell = board[yCoord][0];
+      if (headerCell != "") {
+        pathResult = true;
+        for (let j = 1; j < gridSize; j++) {
+          let compY = yCoord + j * -i;
+          comparisonCell = board[compY][j];
+          if (comparisonCell != headerCell) {
+            pathResult = false;
+          }
         }
+        if (pathResult == true) {
+          return headerCell;
+        }
+      }
+    }
+  } else {
+    for (let i = 0; i < gridSize; i++) {
+      if (direction == "vertical") {
+        headerCell = board[0][i];
+      } else if (direction == "horizontal") {
+        headerCell = board[i][0];
+      }
+      if (headerCell != "") {
+        pathResult = true;
+        for (let j = 1; j < gridSize; j++) {
+          if (direction == "vertical") {
+            comparisonCell = board[j][i];
+          } else if (direction == "horizontal") {
+            comparisonCell = board[i][j];
+          }
+          if (comparisonCell != headerCell) {
+            pathResult = false;
+          }
+        }
+        if (pathResult == true) {
+          return headerCell;
+        }
+      }
+    }
+  }
+  return "";
+}
+
+// returns a string representing the winner, or an empty string if nobody has won
+function checkWins() {
+  winResults = [checkWin("horizontal"), checkWin("vertical"), checkWin("diagonal")];
+
+  let reducer = function (total, num) {
+    if( num === '' && total === '') {
+        return '';
+    } else if (total === '') {
+        return num;
     } else {
-        for (let i = 0; i < gridSize; i++){
-            if (direction == "vertical"){
-                headerCell = board[0][i];
-            } else if (direction == "horizontal"){
-                headerCell = board[i][0];
-            }
-            if (headerCell != ""){
-                pathResult = true;
-                for (let j = 1; j < gridSize; j++){
-                    if (direction == "vertical"){
-                        comparisonCell = board[j][i];
-                    } else if (direction == "horizontal"){
-                        comparisonCell = board[i][j];
-                    }
-                    if (comparisonCell != headerCell){pathResult = false;}
-                }
-                if (pathResult == true){return headerCell;}
-                
-            }  
-        }
+        return total;
     }
-   return false;
+  }
+
+  return winResults.reduce(reducer);
 }
 
-
-function checkWins(){
-    let winResult = ('').concat(checkWin("horizontal"), checkWin("vertical"), checkWin("diagonal")).replace(/false/g,'');
-    return winResult;
+function devDisplay() {
+  if (devMode == true) {
+    print(board);
+    print(availableCells);
+  }
 }
 
-function devDisplay(){
-    if (devMode == true){
-        print(board);
-        print(availableCells);
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+  frameRate(20);
+  generateBoard(gridSize);
+  devDisplay();
+  player = random(players);
+}
+
+function drawGrid() {
+  background(255, 255, 255);
+  let columnWidth = canvasWidth / gridSize;
+  let rowHeight = canvasHeight / gridSize;
+  strokeWeight(4);
+  for (let i = 0; i < gridSize - 1; i++) {
+    let xCoord = columnWidth * (i + 1);
+    line(xCoord, 0, xCoord, canvasHeight);
+  }
+  for (let j = 0; j < gridSize - 1; j++) {
+    let yCoord = rowHeight * (j + 1);
+    line(0, yCoord, canvasWidth, yCoord);
+  }
+}
+
+function drawMarks() {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      if (board[j][i] != "") {
+        var xPos = columnWidth * i + columnWidth / 2;
+        var yPos = rowHeight * j + rowHeight / 2;
+      }
+      if (board[j][i] == players[1]) {
+        noFill();
+        ellipse(xPos, yPos, columnWidth / 2, rowHeight / 2);
+      } else if (board[j][i] == players[0]) {
+        let xLength = columnWidth / 4;
+        let yLength = rowHeight / 4;
+        line(xPos - xLength, yPos - yLength, xPos + xLength, yPos + yLength);
+        line(xPos + xLength, yPos - yLength, xPos - xLength, yPos + yLength);
+      }
     }
+  }
 }
 
-function setup(){
-    createCanvas(canvasWidth, canvasHeight);
-    frameRate(20);
-    generateBoard(gridSize);
-    devDisplay();
-    player = random(players);
+function pushText(message) {
+  textSize(48);
+  fill("red");
+  textAlign(CENTER);
+  text(message, canvasWidth / 2, canvasHeight / 2);
 }
 
-function drawGrid(){
-    background(255,255,255);
-    let columnWidth = canvasWidth / gridSize;
-    let rowHeight = canvasHeight / gridSize;
-    strokeWeight(4);
-    for (let i = 0; i < gridSize - 1; i++){
-        let xCoord = columnWidth * (i + 1);
-        line(xCoord, 0, xCoord, canvasHeight);
+function gameCycle(xCoord, yCoord) {
+  if (board[yCoord][xCoord] == "") {
+    board[yCoord][xCoord] = player;
+    availableCells.splice(availableCells.indexOf([xCoord, yCoord]), 1);
+  } else {
+      print(`An unacceptable move was made at ( ${xCoord}, ${yCoord} )`);
+      return;
+  }
+  turnResult = checkWins();
+  if (turnResult != "") {
+    winStatus = turnResult + " has won!";
+    pushText(winStatus);
+    noLoop();
+  }
+  if (availableCells.length == 0) {
+    pushText("Tie!");
+    noLoop();
+  }
+  if (player == "X") {
+    player = "O";
+  } else {
+    player = "X";
+  }
+}
+
+function draw() {
+  drawGrid();
+  if (keyIsPressed) {
+    if (key == " ") {
+      let aiSelected = aiSelect();
+      gameCycle(aiSelected[0], aiSelected[1]);
     }
-    for (let j = 0; j < gridSize - 1; j++){
-        let yCoord = rowHeight * (j + 1);
-        line(0, yCoord, canvasWidth, yCoord);
-    }
+  }
+  drawMarks();
 }
 
-function drawMarks(){
-    for (let i = 0; i < gridSize; i++){
-        for (let j = 0; j < gridSize; j++){
-            if (board[j][i] != ""){
-                var xPos = (columnWidth * i + columnWidth / 2);
-                var yPos =  (rowHeight * j + rowHeight / 2);
-            }
-            if (board[j][i] == players[1]){
-                noFill();
-                ellipse(xPos, yPos, columnWidth / 2, rowHeight / 2);
-
-            } else if (board[j][i] == players[0]){
-                let xLength = columnWidth / 4;
-                let yLength = rowHeight / 4
-                line(xPos - xLength, yPos - yLength, xPos + xLength, yPos + yLength);
-                line(xPos + xLength, yPos - yLength, xPos - xLength, yPos + yLength);
-            }
-        }
-    }
+function getBoard() {
+  return board;
 }
 
-function pushText(message){
-    textSize(48);
-        fill("red");
-        textAlign(CENTER);
-        text(message, (canvasWidth/2), (canvasHeight/2));
+function getCurrentPlayer() {
+  return player;
 }
 
-function gameCycle(){
-    let cell;
-    let initValue;
-    do {
-        alert((player + "'s Turn"));
-        let xCoord = prompt("x Coord");
-        let yCoord = prompt("y Coord");
-        cell = [xCoord, yCoord];
-        initValue = board[cell[1]][cell[0]];
-        if (initValue == ''){
-            board[cell[1]][cell[0]] = player;
-            availableCells.splice(availableCells.indexOf(cell), 1);
-        } else {
-            alert("Sorry, that space is already taken :(");
-        }
-    } while (initValue != '');
-    turnResult = checkWins();
-    if (turnResult != ''){
-        winStatus = turnResult + " has won!";
-        pushText(winStatus);
-        noLoop();    
-    }
-    if (availableCells.length == 0){
-        pushText("Tie!");
-        noLoop();
-    }
-    if (player == "X"){player = "O"} else {player = "X"};
+function getAvailableCells() {
+  return availableCells;
 }
 
-
-function draw(){
-    drawGrid();
-    gameCycle();
-    drawMarks();
+function getGridSize() {
+  return gridSize;
 }
-
