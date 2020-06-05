58b7cafbc2878e6d214fa7005cd3dfde36656d83
Jamison O'Sullivan <osullivanj@emmanuel.edu>
2020-06-05 10:43:24 -0400

    added text size scaling

--- sketch.js
+++ sketch.js
@@ -191,7 +191,7 @@ function drawGrid() {
 }
 
 function numberCells() {
-  textSize(20);
+  textSize(columnWidth / 4);
   fill('red');
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
